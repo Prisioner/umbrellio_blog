7b51c8730987cbd15c096961a9057f8dd1c513f2
Aleksandr Klimenkov <prisioner@mail.ru>
2018-02-09 01:06:12 +0300

    added post model

---
+++ db/migrate/20180208215824_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.string :ip
+      t.float :rating
+      t.references :user, index: true,  foreign_key: true
+
+      t.timestamps
+    end
+    add_index :posts, :ip
+    add_index :posts, :rating
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208211457) do
+ActiveRecord::Schema.define(version: 20180208215824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "ip"
+    t.float "rating"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ip"], name: "index_posts_on_ip"
+    t.index ["rating"], name: "index_posts_on_rating"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +35,5 @@ ActiveRecord::Schema.define(version: 20180208211457) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
