00d6380af5930bc9f8e380197a7154f94c6b9153
Aleksandr Klimenkov <prisioner@mail.ru>
2018-02-09 01:10:04 +0300

    added rates

---
+++ db/migrate/20180208220825_create_rates.rb
@@ -0,0 +1,10 @@
+class CreateRates < ActiveRecord::Migration[5.1]
+  def change
+    create_table :rates do |t|
+      t.integer :rate
+      t.references :post, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208215824) do
+ActiveRecord::Schema.define(version: 20180208220825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20180208215824) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "rates", force: :cascade do |t|
+    t.integer "rate"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_rates_on_post_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
@@ -36,4 +44,5 @@ ActiveRecord::Schema.define(version: 20180208215824) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "rates", "posts"
 end
