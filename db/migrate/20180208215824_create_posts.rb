8cc5c4555ef87205e5d4a6dc9e47b3535668506e
Aleksandr Klimenkov <prisioner@mail.ru>
2018-02-12 04:07:29 +0300

    update post model migration

--- db/migrate/20180208215824_create_posts.rb
+++ db/migrate/20180208215824_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
       t.string :title
       t.text :body
       t.string :ip
-      t.float :rating
+      t.float :rating, default: 0
       t.references :user, index: true,  foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180208220825) do
     t.string "title"
     t.text "body"
     t.string "ip"
-    t.float "rating"
+    t.float "rating", default: 0.0
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
