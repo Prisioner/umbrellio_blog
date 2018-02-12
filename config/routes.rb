a5d76a574b284c6ebe21b0f447d0c601a2c56361
Aleksandr Klimenkov <prisioner@mail.ru>
2018-02-12 17:27:09 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :posts, only: [:create, :top] do
+        get :top, on: :collection
+
+        resources :rates, only: [:create]
+      end
+
+      resources :users, only: [:ip_groups] do
+        get :ip_groups, on: :collection
+      end
+    end
+  end
 end
