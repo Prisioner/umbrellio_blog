fb9720a35a0083b65679df4ffd7eef4e19fa9fd6
Aleksandr Klimenkov <prisioner@mail.ru>
2018-02-12 04:08:08 +0300

    update post & user models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :rates
+  has_many :rates, dependent: :destroy
+
+  delegate :username, to: :user
+
+  scope :top, ->(count) { order_by(:rating).limit(count) }
 end
